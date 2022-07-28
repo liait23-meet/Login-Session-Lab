7643a0b711c71508653d99e57194bd873b3618f8
liait23-meet <109908141+liait23-meet@users.noreply.github.com>
2022-07-28 15:46:34 +0300

    Lab is done

--- Login-Session-Lab/login_session.py
+++ Login-Session-Lab/login_session.py
@@ -4,10 +4,21 @@ from flask import session as login_session
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-@app.route('/', ) # What methods are needed?
+@app.route('/', methods=['GET', 'POST'])
 def home():
-	
-	return render_template('home.html')
+	if request.method == 'POST':
+		try:
+			message = request.form['message']
+			login_session['message'] = message
+			name = request.form['name']
+			login_session['name'] = name
+			age = request.form['age']
+			login_session['age'] = age
+			return render_template('thanks.html')
+		except:
+			return render_template('error.html')
+	else:
+		return render_template('home.html')
 
 
 @app.route('/error')
@@ -19,7 +30,7 @@ def error():
 @app.route('/display')
 def display():
 
-	return render_template('display.html', ) # What variables are needed?
+	return render_template('display.html', message = login_session['message'], name  =login_session['name'] , age =  login_session['age']) # What variables are needed?
 
 
 @app.route('/thanks')
